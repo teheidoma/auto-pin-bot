a86a4dace0a58aaa17b684a47dd81b35625a24c3
Андрей Новаковский <novakovskii2002@gmail.com>
2020-08-07 01:34:18 +0300

    fix image bug

--- scripts/build_and_push_image.sh
+++ scripts/build_and_push_image.sh
@@ -6,6 +6,8 @@ gradle bootBuildImage
 
 docker push docker.pkg.github.com/teheidoma/auto-pin-bot/auto-pin-bot:"$version"
 
+docker tag docker.pkg.github.com/teheidoma/auto-pin-bot/auto-pin-bot:"$version" docker.pkg.github.com/teheidoma/auto-pin-bot/auto-pin-bot:latest
+
 docker push docker.pkg.github.com/teheidoma/auto-pin-bot/auto-pin-bot:latest
 
 echo "done!"
